e51e989a79a24bc6ee098ccd480a5b2d2d9a51dc
Anirudh Chaudhary <32309907+Always2KSpiner@users.noreply.github.com>
2020-10-12 00:23:55 -0700

    MID SEM PROJECT

---
+++ R171217008/School-web-application-master/public/script/index.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var buttonLength = document.querySelectorAll("#call-to-action .button").length;
+// var button = document.querySelectorAll("#call-to-action .button");
+// var i;
+// for(i = 0; i < buttonLength; i++){
+//     button[i].addEventListener("click", function() {
+//         var button = this;
+//         animateButton(button)
+//     });
+// }
+
+// function animateButton(btn){
+//    btn.classList.add("animate");
+//    setTimeout(function() {
+//     btn.classList.remove("animate");
+//    }, 200);
+// }
+
+// var albumArray = ["album-1", "album-2", "album-3", "album-4", "album-5",];
+// var photo = document.querySelector(".photo");
+
+// var albumLength = document.querySelectorAll(".album").length;
+// for(var i = 0; i < albumLength; i++){
+//     document.querySelectorAll(".album")[i].addEventListener("click", function() {
+//         albumAnimate(this);
+//     });
+// }
+
+// function albumAnimate(evt) {
+//     var className = evt.id;
+//     switch(className){
+//         case "jet":{
+            
+//         }
+//     }
+// }
+
+
